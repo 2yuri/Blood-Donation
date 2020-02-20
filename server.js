11b1b914d8ef8fecbf54e725b4b77a67628119d7
hyperyuri <yuri.snp@hotmail.com>
2020-02-20 07:22:30 -0300

    Back

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ const server = express()
 // configurando o server pra apresentar arquivos extras
 server.use(express.static('public'))
 
+// habilitar body do form
+server.use(express.urlencoded({ extended: true }))
+
 //configurando a template engine
 const nunjucks = require("nunjucks")
 nunjucks.configure("./", {
   express: server,
-  noCache: true,
+  noCache:true,
 })
 
 // lista dos doadores -- array
@@ -25,18 +28,34 @@ const donors = [
   },
   {
     name: "Robson Junior",
-    blood: "A+"
+    blood: "O+"
   },
   {
     name: "Mayk Brito",
-    blood: "O+"
-  }
+    blood: "A-"
+  },
 ]
 
 
 // configurar a apresentação da página
 server.get("/", function(req, res) {
   return res.render("index.html", { donors })
+})
+
+server.post("/", function(req, res) {
+  //pegar dados do formulario
+  const name = req.body.name
+  const email = req.body.email
+  const blood = req.body.blood
+
+  //push - coloca valor dentro do array
+  donors.push({
+    name: name,
+    blood: blood
+  })
+
+  return res.redirect("/")
+
 })
 
 //ligar servidor e permitir acesso a port 3001
